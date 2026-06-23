8493617d79ea4bc8c940aabb577d8ef2af18702d
lazy-cat-cult <lazycatcult@github.com>
2026-06-23 14:13:54 +0300

    Replace Google Fonts URLs with local @fontsource packages; switch display font to Ubuntu Condensed; preset name UI refinements

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/ubuntu-condensed/index.css';
+import '@fontsource-variable/space-grotesk/index.css';
+import '@fontsource-variable/jetbrains-mono/index.css';
 import { render } from 'preact';
 import { App } from './app';
 import './style.css';
